0d48454fd4328f74bb640fc62d410e05cbcd9f2c
Franky <frankyan2001@gmail.com>
2025-07-04 12:00:00 -0400

    Creando el switch de modo oscuro

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import './App.css';
 
 import InstagramCarousel from './components/InstagramCarousel';
 import PresentationCard from './components/PresentationCard';
+import DarkLightModeSwitch from './components/DarkLighModeSwitch';
 
 
 function App() {
   return (
     <div className='App'>
-      <PresentationCard/>
+      <DarkLightModeSwitch/>
     </div>
   );
 }
 
 export default App;
-//<InstagramCarousel />
