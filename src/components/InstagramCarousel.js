0ddca1ef4d2f5f0f7bfbe75a15f7915e8c4b899b
Franky <frankyan2001@gmail.com>
2025-07-14 16:16:28 -0400

    Instagram carousel component added

---
+++ src/components/InstagramCarousel.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Slider from 'react-slick/lib/slider';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+
+const InstagramCarousel = () => {
+};
+
+export default InstagramCarousel;
