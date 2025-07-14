55916b04f29b40308594d00896165d683a741a20
Franky <frankyan2001@gmail.com>
2025-07-14 16:17:08 -0400

    images at Instagram carousel component added

--- src/components/InstagramCarousel.js
+++ src/components/InstagramCarousel.js
@@ -5,6 +5,13 @@ import 'slick-carousel/slick/slick-theme.css';
 
 
 const InstagramCarousel = () => {
+  const images = [
+    'https://images.unsplash.com/photo-1544005313-94ddf0286df2?w=150&h=150&fit=crop&crop=face',
+    'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?w=150&h=150&fit=crop&crop=face',
+    'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?w=150&h=150&fit=crop&crop=face',
+    'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?w=150&h=150&fit=crop&crop=face',
+  ]
+
 };
 
 export default InstagramCarousel;
