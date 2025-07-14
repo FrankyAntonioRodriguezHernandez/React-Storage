9f65fd4ed7486d4f810893b698030ecd730be892
Franky <frankyan2001@gmail.com>
2025-07-14 16:19:25 -0400

    Instagram carousel component added at app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 
-
+import InstagramCarousel from './components/InstagramCarousel';
 
 function App() {
   return (
     <div className='App'>
-      
+      <InstagramCarousel />
     </div>
   );
 }
